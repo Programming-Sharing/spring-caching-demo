68c6bfb2edad777f62ab23f7b6e4c4609e69f23e
Thanh Tran <Thanh.Tran@terralogic.com>
2020-10-01 11:19:47 +0700

    Initial commit for spring-caching-demo

---
+++ src/main/java/com/programmingsharing/demo/SpringCachingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.programmingsharing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCachingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCachingDemoApplication.class, args);
+	}
+
+}
