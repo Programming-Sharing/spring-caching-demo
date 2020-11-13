652e9d996f5ddb14a2a9b7c2d40daa141a3e5b46
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-11-13 12:04:22 +0700

    enable command line runner

--- src/main/java/com/programmingsharing/demo/SpringCachingDemoApplication.java
+++ src/main/java/com/programmingsharing/demo/SpringCachingDemoApplication.java
@@ -1,13 +1,23 @@
 package com.programmingsharing.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCachingDemoApplication {
+public class SpringCachingDemoApplication implements CommandLineRunner{
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringCachingDemoApplication.class);
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCachingDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {		
+		
 	}
 
 }
