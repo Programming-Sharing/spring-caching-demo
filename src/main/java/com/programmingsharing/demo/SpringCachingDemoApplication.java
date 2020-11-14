89bec5803754a23a1bdd3262ac776c714fb8e166
Thanh Tran <tranthanhdeveloper@gmail.com>
2020-11-14 23:37:47 +0700

    Integrate Hazelcast as Cache Manager

--- src/main/java/com/programmingsharing/demo/SpringCachingDemoApplication.java
+++ src/main/java/com/programmingsharing/demo/SpringCachingDemoApplication.java
@@ -2,22 +2,38 @@ package com.programmingsharing.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import com.programmingsharing.demo.services.BookService;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringCachingDemoApplication implements CommandLineRunner{
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringCachingDemoApplication.class);
 
-
+	@Autowired
+	private BookService bookService;
+	
+	@Autowired
+	private ConfigurableApplicationContext applicationContext;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCachingDemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {		
+		LOGGER.info(bookService.findBook("0134685997").toString());
+		LOGGER.info(bookService.findBook("0134685997").toString());
+		LOGGER.info(bookService.findBook("0134685997").toString());
+		LOGGER.info(bookService.findBook("0134685997").toString());
 		
+		applicationContext.close();
 	}
 
 }
